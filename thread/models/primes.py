1ebadb8f67053c2b47efc8c422f154d2a3ce6618
Burak Köse <burakks41@gmail.com>
2016-03-12 13:35:34 +0200

    style fix

--- thread/models/primes.py
+++ thread/models/primes.py
@@ -6,22 +6,17 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
-'''veri tabanı ile ilgili işlemler'''
 
 class Primes(Base):
-    ''' veritabanındaki tablo için yaratılmış sınıf
-    '''
     __tablename__ = 'primes'
     id = Column(Integer, primary_key=True)
     num = Column(Integer, nullable=False)
 
 
 class PrimeController:
-    ''' controller yani veritabanından bunun sayesinde veri çekiyoruz
-    '''
+
     @staticmethod
     def get_primes_between(start, stop):
-        '''start ile stop arasındaki sayıları getir'''
         return list(map(lambda prime: prime.num,
                         session.query(Primes).filter(
                             and_(start <= Primes.num,
@@ -30,19 +25,16 @@ class PrimeController:
 
     @staticmethod
     def get_latest_prime_num():
-        '''veritabanındaki son asal sayıyı getir'''
         return session.query(Primes).order_by(Primes.id.desc()).first().num
 
     @staticmethod
     def init_table():
-        '''veritabanı yoksa yeri tabanı oluştur'''
         for prime in list(map(lambda x: Primes(num=x), [2, 3, 5, 7, 11, 13])):
             session.add(prime)
             session.commit()
 
     @staticmethod
     def add_table(num):
-        '''veritabanına veri ekle'''
         session.add(Primes(num=num))
         session.commit()
 
--- thread/primer.py
+++ thread/primer.py
@@ -1,6 +1,5 @@
 import sys
 import threading
-
 from PyQt4 import QtGui, QtCore
 
 from gui.mygui import Ui_MainWindow
@@ -10,39 +9,24 @@ result = None
 search = True
 
 
-# Eşit bölmek için bir fonksiyon
 def partition(lst, n):
     division = len(lst) / float(n)
     return [lst[int(round(division * i)): int(round(division * (i + 1)))] for i in range(n)]
 
 
 def worker(id2, check, range_):
-    '''
-İşçi fonksiyonu, her thread bu fonksiyonu çalıştırıyor.
-id2 = thread numarası
-check = aranan sayı
-range_ = arama aralığı
-'''
     global result
     global search
     for i in list(range_):
         if not search:
             break
-        if check % i == 0:  # mod 0 ise sayı asal değildir
+        if check % i == 0:
             search = False
-            result = (id2, i)  # asallığı bozanı global deişkene ata
+            result = (id2, i)
             break
 
 
-'''
-Form ekranı için oluşturulmuş sınıf'''
-
-
 class MyApp(QtGui.QMainWindow, Ui_MainWindow):
-    ''' Ekranda gösterilen bilgiler
-    no_prime_text = asal değil diyor ve şu thread sunu bozdu diyor
-    prime_test = asal diyor
-    '''
     no_prime_text = "{} is not prime number because of Thread{} and {}"
     prime_text = "{} is prime number"
 
@@ -54,67 +38,58 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         self.ui.setupUi(self)
         self.ui.thread_table.setColumnCount(4)
         self.ui.thread_table.setHorizontalHeaderLabels(
-                ['Thread', 'Start', 'Stop', 'Durum'])
+            ['Thread', 'Start', 'Stop', 'Durum'])
         self.ui.start_button.clicked.connect(self.start)
         self.ui.stop_button.clicked.connect(self.stop)
 
     def start(self):
-        '''start butonuna bastıgında bu çalışıyor yani iş burada yapılıyor
-        '''
         global result, search
-        self.is_run = True  # çalışıyor mu
-        latest = PrimeController.get_latest_prime_num() + 2  # veri tabanında kayıtlı son asal sayı + 2
+        self.is_run = True
+        latest = PrimeController.get_latest_prime_num() + 2
         while self.is_run:
-            if latest % 2 == 0:  # sayı çift ise takma
-                latest += 1  # latest aranan sayı
+            if latest % 2 == 0:
+                latest += 1
             QtCore.QCoreApplication.processEvents()
-            self.set_searching_number(latest)  # ekrana aranan sayının bilgisini gösteriyor
+            self.set_searching_number(latest)
 
-            sqrt = int(latest ** 0.5) + 1  # sayıın karekökünün 1 fazlası
-            # veritabanında 2 ile yukarıda bulunan sonuca kadarki
-            # olan asal sayılar getiriliyor
+            sqrt = int(latest ** 0.5) + 1
             primesdb = PrimeController.get_primes_between(2, sqrt)
-            if len(primesdb) <= 10:  # eğer asal sayı sayısı 100 den küçükse
-                threads = [None] * 2  # 2 tane thread yarat
-            else:  # 100 den fazla ise
-                # dökümanda verilen formuldeki sayı kadar thread yarat
+            if len(primesdb) <= 10:
+                threads = [None] * 2
+            else:
+
                 threads = [None] * (len(primesdb) // 10 + 1)
 
             limits = partition(primesdb, len(threads))
-            # yukarıda hangi threadlerin hangi aralıklara bakacagı ayarlanıyor
-            for i in range(len(threads)):  # threadleri yaratıyor
-                threads[i] = threading.Thread(
-                        target=worker, args=(i, latest, limits[i],))
-                threads[i].start()  # threadleri çalıştır
 
-            self.update_table(len(threads), limits)  # tabloda bilgi göster ekranda
+            for i in range(len(threads)):
+                threads[i] = threading.Thread(
+                    target=worker, args=(i, latest, limits[i],))
+                threads[i].start()
+
+            self.update_table(len(threads), limits)
 
             for i in threads:
-                i.join()  # tüm threadler birbirini bekleyecekler yani hepsi bitene kadar program bekleyecek
-
-            if result is None:  # veri tabanına ekleniyor. Farklı threadde
+                i.join()
+            if result is None:
                 PrimeController.add_table(latest)
 
-            self.update_table(len(threads), limits, result)  # yine ekrandaki yerlerde bilgiler gösteriliyor
-            self.set_result_information(latest, result)  # sayı hakkıda bilgi gösteriliyor asal mı değil mi diye
-            result, search, latest = None, True, latest + 1  # tekrar araştırma için her şey sıfırlanıyor
+            self.update_table(len(threads), limits, result)
+            self.set_result_information(latest, result)
+            result, search, latest = None, True, latest + 1
 
     def stop(self):
-        '''stop bastığında bu olacak'''
         self.is_run = False
 
     def set_searching_number(self, num):
-        '''aranan numarayı ekranda göster'''
         self.ui.searching_number.display(num)
 
     def set_result_information(self, num, res):
-        '''sayı hakkında bilgi göster asal mı değil mi gibi'''
         text = self.prime_text.format(num) if res is None else \
             self.no_prime_text.format(num, res[0], res[1])
         self.ui.result_label.setText(text)
 
     def update_table(self, thread_count, limits, arg=None):
-        '''ekrandaki tabloyu güncelliyor'''
         thread_id = -1 if arg is None else arg[0]
 
         self.ui.thread_table.clearContents()
@@ -123,17 +98,14 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
             limits[i] = limits[i] * 2 if len(limits[i]) == 1 else limits[i]
             self.ui.thread_table.setItem(i, 0, QtGui.QTableWidgetItem(str(i)))
             self.ui.thread_table.setItem(
-                    i, 1, QtGui.QTableWidgetItem(str(limits[i][0])))
+                i, 1, QtGui.QTableWidgetItem(str(limits[i][0])))
             self.ui.thread_table.setItem(
-                    i, 2, QtGui.QTableWidgetItem(str(limits[i][-1])))
+                i, 2, QtGui.QTableWidgetItem(str(limits[i][-1])))
             column4 = True if (thread_id == i) else False
             self.ui.thread_table.setItem(
-                    i, 3, QtGui.QTableWidgetItem(str(column4)))
+                i, 3, QtGui.QTableWidgetItem(str(column4)))
 
 
-'''
-Main fonksiyonu
-'''
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     window = MyApp()
